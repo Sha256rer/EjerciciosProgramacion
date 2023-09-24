9a96a9ed1ec7c5c550677b8ef80f526871579b9f
Sharerp <sharerp@gmail.com>
2023-09-24 23:03:49 +0200

    Added two new classes

--- primerosProgramas/src/primerosProgramas/Ejercicio2.java
+++ primerosProgramas/src/primerosProgramas/Ejercicio2.java
@@ -3,7 +3,8 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		int a= 1, b=4, c=10;
-		boolean check = a==b && a != c;
+		boolean check = (a==b && a != c);
+		System.out.println(check);
 		
 		
 	}
